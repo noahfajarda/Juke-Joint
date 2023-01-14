e682b8e062c73dfed5ad3a270168c6be260c9350
Anas Qazza <anasqazza@gmail.com>
2023-01-14 10:08:58 -0800

    This app works on this commit

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,12 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
+console.log(process.env.DB_NAME);
 
 // create connection to our db, JAWSDB_URL env variable is provided by heroku in production environment
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: "localhost",
-        dialect: "mysql",
-        port: 8889,
-    }
-);
+const sequelize = new Sequelize("music_db", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+  port: 3306,
+});
 
 module.exports = sequelize;
