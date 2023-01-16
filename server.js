6293bd56d11ae6d0645bd5978e27cb30e217d17d
Anas Qazza <anasqazza@gmail.com>
2023-01-16 14:59:34 -0800

    Updated Sign up/login sheet

--- server.js
+++ server.js
@@ -31,18 +31,18 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 //session configuration object, refer to express-session documentation to modify configs
 const sess = {
-    secret: "Super secret secret",
-    cookie: {
-        maxAge: 60 * 60 * 1000, //this is 1 hour, adjust if you need to
-        httpOnly: true,
-        secure: false,
-        sameSite: "strict",
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-    }),
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 60 * 60 * 1000, //this is 1 hour, adjust if you need to
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 //apply session middleware
@@ -57,50 +57,47 @@ const {getAccessToken, accessTokenExpired} = require("./utils/retrieve-authoriza
 const {search_for_track, search_for_album, search_for_artist, retrieve_lyrics, colors} = require("./utils/fetch-API-data-&-colors");
 
 async function main() {
-    // accessToken = ACTUAL access token
-    const accessToken = await getAccessToken();
+  // accessToken = ACTUAL access token
+  const accessToken = await getAccessToken();
 
-    // testing each API fetch
-    try {
-        // ARTIST FETCH
-        const artistRes = await search_for_artist(accessToken, "anderson Paak");
-        const artistData = await artistRes.json();
-        // extract necessary artist data
-        const artistName = artistData.artists.items[0].name;
-        const artistId = artistData.artists.items[0].id;
-        // log extracted data
-        console.log(`\nArtist Name: ${artistName}\nArtist ID: ${artistId}`);
+  // testing each API fetch
+  try {
+    // ARTIST FETCH
+    const artistRes = await search_for_artist(accessToken, "anderson Paak");
+    const artistData = await artistRes.json();
+    // extract necessary artist data
+    const artistName = artistData.artists.items[0].name;
+    const artistId = artistData.artists.items[0].id;
+    // log extracted data
+    console.log(`\nArtist Name: ${artistName}\nArtist ID: ${artistId}`);
 
-        // ALBUM FETCH
-        const albumRes = await search_for_album(accessToken, "Camp");
-        const albumData = await albumRes.json();
-        // extract necessary album data
-        const albumName = albumData.albums.items[0].name;
-        const albumArtist = albumData.albums.items[0].artists[0].name;
-        const albumId = albumData.albums.items[0].id;
-        // log extracted data
-        console.log(
-            `\nAlbum Name: ${albumName} {by: ${albumArtist}}\nAlbum ID: ${albumId}`
-        );
+    // ALBUM FETCH
+    const albumRes = await search_for_album(accessToken, "Camp");
+    const albumData = await albumRes.json();
+    // extract necessary album data
+    const albumName = albumData.albums.items[0].name;
+    const albumArtist = albumData.albums.items[0].artists[0].name;
+    const albumId = albumData.albums.items[0].id;
+    // log extracted data
+    console.log(
+      `\nAlbum Name: ${albumName} {by: ${albumArtist}}\nAlbum ID: ${albumId}`
+    );
 
-        // TRACK FETCH
-        const trackRes = await search_for_track(
-            accessToken,
-            "Me and Your Mama"
-        );
-        const trackData = await trackRes.json();
-        // extract necessary track data
-        // const trackArt = trackData.tracks.items[0].album.images[3].url;
-        const trackArt = trackData.tracks.items[0].album.images[2].url;
-        const trackName = trackData.tracks.items[0].name;
-        const trackArtist = trackData.tracks.items[0].artists[0].name;
-        const trackId = trackData.tracks.items[0].id;
-        // log extracted data
-        console.log(`\nTrack Name: ${trackName} {by: ${trackArtist}}`);
-        console.log(`Track ID: ${trackId}`);
-    } catch (err) {
-        accessTokenExpired();
-    }
+    // TRACK FETCH
+    const trackRes = await search_for_track(accessToken, "Me and Your Mama");
+    const trackData = await trackRes.json();
+    // extract necessary track data
+    // const trackArt = trackData.tracks.items[0].album.images[3].url;
+    const trackArt = trackData.tracks.items[0].album.images[2].url;
+    const trackName = trackData.tracks.items[0].name;
+    const trackArtist = trackData.tracks.items[0].artists[0].name;
+    const trackId = trackData.tracks.items[0].id;
+    // log extracted data
+    console.log(`\nTrack Name: ${trackName} {by: ${trackArtist}}`);
+    console.log(`Track ID: ${trackId}`);
+  } catch (err) {
+    accessTokenExpired();
+  }
 }
 
 main();
@@ -108,7 +105,7 @@ main();
 // change to { force: false } once we figure out our database strucutre
 // { force: true } used for development and testing database structure
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () =>
-        console.log(`App listening at http://localhost:${PORT} 🚀`)
-    );
+  app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+  );
 });
--- utils/retrieve-authorization-token.js
+++ utils/retrieve-authorization-token.js
@@ -12,32 +12,30 @@ const client_secret = process.env.CLIENT_SECRET; // Spotify Client_Secret
 const musixmatch_api_key = process.env.MUSIXMATCH_API_KEY; // Spotify Musixmatch API key
 // "retrieve ACCESS TOKEN" function
 async function getAccessToken() {
-    const testing = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: {
-            // taken from spotify api docs
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: `Basic ` + btoa(client_id + ":" + client_secret),
-        },
-        body: "grant_type=client_credentials",
-    });
-    const filteredResults = await testing.json();
-    console.log(
-        `\n\n\n\n\n ${colors.yellow}Line 24 retrieve-authorizationToken.js:${colors.white}`
-    );
-    console.log(filteredResults); // access_token & expires_in
-    const accessToken = filteredResults.access_token;
-    console.log(`${colors.magenta}Access Token ${accessToken}${colors.white}`); // recolor access token
-    return accessToken;
+  const testing = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      // taken from spotify api docs
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ` + btoa(client_id + ":" + client_secret),
+    },
+    body: "grant_type=client_credentials",
+  });
+  const filteredResults = await testing.json();
+  console.log(
+    `\n\n\n\n\n ${colors.yellow}Line 24 retrieve-authorizationToken.js:${colors.white}`
+  );
+  console.log(filteredResults); // access_token & expires_in
+  const accessToken = filteredResults.access_token;
+  console.log(`${colors.magenta}Access Token ${accessToken}${colors.white}`); // recolor access token
+  return accessToken;
 }
 
 function accessTokenExpired() {
-    // colored error message
-    console.log("\x1b[31m" + "\nThe access token expired.");
-    console.log(
-        "\x1b[31m" + "Copy the access token again printed from console."
-    );
-    // console.log(err);
+  // colored error message
+  console.log("\x1b[31m" + "\nThe access token expired.");
+  console.log("\x1b[31m" + "Copy the access token again printed from console.");
+  // console.log(err);
 }
 
 module.exports = { getAccessToken, accessTokenExpired };
