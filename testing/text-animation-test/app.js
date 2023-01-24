b242be5fc00bad4f25f370b2207f061a9a40b0a3
Anas Qazza <anasqazza@gmail.com>
2023-01-23 20:14:22 -0800

    Working updated

--- testing/text-animation-test/app.js
+++
@@ -1,7 +0,0 @@
-console.log("lost");
-
-const logo = document.querySelectorAll("#logo path");
-
-for (let i = 0; i < logo.length; i++) {
-    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-}
--- utils/retrieve-authorization-token.js
+++ utils/retrieve-authorization-token.js
@@ -12,7 +12,6 @@ const client_secret = process.env.CLIENT_SECRET; // Spotify Client_Secret
 const musixmatch_api_key = process.env.MUSIXMATCH_API_KEY; // Spotify Musixmatch API key
 // "retrieve ACCESS TOKEN" function
 async function getAccessToken() {
-<<<<<<< HEAD
   const testing = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: {
@@ -30,24 +29,6 @@ async function getAccessToken() {
   const accessToken = filteredResults.access_token;
   console.log(`${colors.magenta}Access Token ${accessToken}${colors.white}`); // recolor access token
   return accessToken;
-=======
-    const testing = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: {
-            // taken from spotify api docs
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: `Basic ` + btoa(client_id + ":" + client_secret),
-        },
-        body: "grant_type=client_credentials",
-    });
-    const filteredResults = await testing.json();
-
-    // RETRIEVING THE ACCESS TOKEN
-    console.log(filteredResults); // access_token & expires_in
-    const accessToken = filteredResults.access_token;
-    console.log(`${colors.magenta}Access Token ${accessToken}${colors.white}\n`); // recolor access token
-    return accessToken;
->>>>>>> 1cb236c33043b466a4bcdcd183fb5205b27037bb
 }
 
 function accessTokenExpired() {
