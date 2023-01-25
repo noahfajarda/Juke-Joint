d586946609c0d15cc5bd51e6b17da70f8c2f4b1a
Anas Qazza <anasqazza@gmail.com>
2023-01-24 20:42:32 -0800

    worked on handlebars

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,23 @@ require("dotenv").config();
 console.log(process.env.DB_NAME);
 
 // create connection to our db, JAWSDB_URL env variable is provided by heroku in production environment
+<<<<<<< Updated upstream
 const sequelize = new Sequelize("music_db", "root", "root", {
   host: "localhost",
   dialect: "mysql",
   port: 3306,
 });
+=======
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+>>>>>>> Stashed changes
 
 module.exports = sequelize;
