72b8796c0fbdab58fb41ae9f634a136a5314ebb5
Noah Fajarda <noahfajarda@gmail.com>
2023-01-27 13:39:37 -0800

    changed connection port

--- config/connection.js
+++ config/connection.js
@@ -15,7 +15,7 @@ if (process.env.JAWSDB_URL) {
         {
             host: "localhost",
             dialect: "mysql",
-            port: process.env.DB_PORT || 3306,
+            port: process.env.DB_PORT || 8889,
         }
     );
 }
